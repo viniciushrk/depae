61e7d186e46e5c976d8b23aac91e49802aae5ca0
SrHenry <lucapvh949@gmail.com>
2018-12-19 18:13:32 -0400

    Vinicius, vê ai agora no pag4.php e o assets/js/lixo.js

--- assets/js/lixo.js
+++ assets/js/lixo.js
@@ -1,21 +1,21 @@
 //import 'jquery.js' or 'jquery.min.js' first!
 function ns() {
-    $('.fufufufufu').remove();
+    $('.apagarAposSelecaoNivelFalta').remove();
     var nivelDeFalta = $('#nivel_falta')[0].value;
     switch (nivelDeFalta) {
         case 0:
             break;
         case 1:
-            $('[joao=x2]').remove();
-            $('[joao=x3]').remove();
+            $('[nivel_falta=2]').remove();
+            $('[nivel_falta=3]').remove();
             break;
         case 2:
-            $('[joao=x1]').remove();
-            $('[joao=x3]').remove();
+            $('[nivel_falta=1]').remove();
+            $('[nivel_falta=3]').remove();
             break;
         case 3:
-            $('[joao=x1]').remove();
-            $('[joao=x2]').remove();
+            $('[nivel_falta=1]').remove();
+            $('[nivel_falta=2]').remove();
             break;
 
     }
