32715fc3721a281f58439e7f70b885c374a74f09
SrHenry <lucapvh949@gmail.com>
2018-12-20 12:59:52 -0400

    Odeio PHP e seus PDOs que não funcionam no linux

--- classes/Conexao.php
+++ classes/Conexao.php
@@ -10,28 +10,30 @@ class Conexao
 {
 
     private $user = 'root';
-    private $senha = '';
+    private $senha = 'LuizGamer_13';
     private $db = 'depae';
     private $host = 'localhost:3306';
+    private $port = "3306";
 
     /**
      * @return PDO|string
      */
+
+
+
     public function conecta()
     {
         try {
-            $con = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->senha);
+            $con = new PDO("mysql:host=$this->host;port=$this->port;dbname=$this->db", $this->user, $this->senha, array(PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_CASE => PDO::CASE_LOWER, PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
             $con->query("SET NAMES utf8");
             return $con;
         } catch (PDOException $e) {
-            try{
-                $con = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, "LuizGamer_13");
-                $con->query("SET NAMES utf8");
-                return $con;
-            }catch(PDOException $e) {
-                return $e->getMessage();
-            }
+            echo $e->getMessage();
+            return $e->getMessage();
 
         }
     }
 }
+
+
