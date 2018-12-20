b179faf44040abc2db454756f33a1d753b6445b6
viniciushrk <viniciusrosa444@gmail.com>
2018-12-20 11:03:22 -0300

    enviando esqueci.php

--- esqueci.php
+++ esqueci.php
@@ -1,22 +1,26 @@
+<?php
 
+require_once "footer.php";
+
+?>
 <head>
     <meta charset="UTF-8">
 
 </head>
 <body>
-    <form method="post">
+    <form method="get">
         <h1>Atualizar usuário</h1><br>
 
         <div class="form-row">
 
             <div class="form-group col-lg-6">
                 <label for="nome">Nome</label>
-                <input type="text" class="form-control" name="nome" id="nome" placeholder="nome" value="<?php echo $servidor->getNome();?>" required>
+                <input type="text" class="form-control" name="nome" id="nome" placeholder="nome" value="<?php ?>" required>
             </div>
 
             <div class="form-group col-lg-6">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="email" value="<?php echo $servidor->getLoginEmail();?>" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="email" value="" required>
             </div>
         </div>
     </form>
