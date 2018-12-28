46884079372f9585118df133173557addaaaf136
viniciushrk <viniciusrosa444@gmail.com>
2018-12-28 16:23:24 -0300

    esqueci.php

--- esqueci.php
+++ esqueci.php
@@ -1,35 +1,38 @@
-<<<<<<< HEAD
+
 
 <body>
-    <form method="get">
-=======
-<div>
->>>>>>> 4b6a1ccfc8791da56c8f2bccb55ddc130dd4b434
-        <h1>Atualizar usuário</h1><br>
 
+<br>
+<br>
+<br>
+<div class="card">
+    <div class="card-body">
+
+        <h1>Esqueci a senha</h1><br>
+        <form method="get" action="novasenha.php">
         <div class="form-row">
-
             <div class="form-group col-lg-6">
-<<<<<<< HEAD
-                <label for="nome">Siape</label>
-                <input type="text" class="form-control" name="nome" id="nome" placeholder="nome" value="<?php ?>" required>
+                <label for="siape">Siape</label>
+                <input type="text" class="form-control" name="siape" id="siape" placeholder="siape" value="<?php ?>" required>
+                <br>
             </div>
-
-=======
+            <!--   <div class="form-group col-lg-6">
                 <label for="nome">Nome</label>
-                <input type="text" class="form-control" name="nome" id="nome" placeholder="nome" value="<?php ?>" required>
-            </div>
-
-
->>>>>>> 4b6a1ccfc8791da56c8f2bccb55ddc130dd4b434
+                <input type="text" class="form-control" name="nome" id="nome" placeholder="nome" value="<?php /**/?>" required>
+            </div>-->
             <div class="form-group col-lg-6">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="email" value="" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="email" value="">
+            </div>
+            <div class="form-group">
+                <input class="btn btn-success float-right" type="submit" name="verificar" value="verificar" required>
+                <a href="index.php?page=novasenha"></a>
+
+                <!--<button class="btn btn-success float-lg-right float-right" type="submit" name="verificar" value="verificar" href="novasenha.php">Verificar</button>-->
+                <a href="index.php?page=<?php if (isset($_GET['http_referer'])) { echo $_GET['http_referer'];} ?>"><button type="button" class="btn btn-danger float-left" name="cancelar">Cancelar</button></a>
             </div>
         </div>
-<<<<<<< HEAD
     </form>
-</body>
-=======
+    </div>
 </div>
->>>>>>> 4b6a1ccfc8791da56c8f2bccb55ddc130dd4b434
+</body>
