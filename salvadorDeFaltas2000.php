cfd84d875e08e99cccbf81e28e6c3dfec59117fd
Luiz Paulo Martins Beleza <lmartinsbeleza@gmail.com>
2019-01-04 16:16:29 -0400

    pag para salvar as coisas do cadastro de falta

--- salvadorDeFaltas2000.php
+++ salvadorDeFaltas2000.php
@@ -6,6 +6,36 @@
  * Time: 14:14
  */
 
+require_once "classes/Servidor.php";
+require_once "classes/Faltas.php";
+require_once "classes/Nivel_falta.php";
+require_once "classes/Aluno.php";
+require_once "classes/Curso.php";
+require_once "classes/Turno.php";
+require_once "classes/Turma.php";
+require_once "classes/Motivo.php";
+session_start();
 
+$servidor = new Servidor();
+$Faltas = new Faltas();
+$Nivel_falta = new Nivel_falta();
+$Aluno = new Aluno();
+$Curso = new Curso();
+$Turno = new Turno();
+$Turma = new Turma();
+$Motivo = new Motivo();
+
+
+$Aluno->selecionaAlunosPorNome('nome');
+$Turma->selecionaPorIdTurma('idturma');
+$Curso->selecionaPorIdCurso($Turma->getCursoIdCurso());
+$Turno->seleciona($Turma->getTurnoIdTurno());
+//$Nivel_falta->selecionaPorNivelFalta($_POST('nivel_falta'));
+//$Motivo->selecionaMotivoPorNome_e_NivelFalta($_POST('nome'),$Nivel_falta->getNivelFalta());
+//$Motivo->selecionaMotivoPorNome($_POST('nome'));
+$Faltas->setAlunoNumMatricula($Aluno->getNumMatricula());
+$Faltas->setMotivoIdMotivo($Motivo->getIdMotivo());
+$Faltas->atualizar();
+header("Location: index.php?page=pag6");
 
 ?>
